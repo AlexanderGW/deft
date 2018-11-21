6a6572f50d9f391d44b61e5fa1495d24304d7f30
Alexander Gailey-White <alexgw@gmail.com>
2018-11-21 23:08:29 +0000

    Add NPM package file

---
+++ public/asset/cache/2e4f500ff67b405c7e58e143bf77618e.js
@@ -0,0 +1,3 @@
+/* (1/1) plugin/example/asset/js/main.js */
+console.log('hi');
+
---
+++ public/index.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Snappy, a PHP framework for PHP 5.3+
+ *
+ * @author Alexander Gailey-White <alex@gailey-white.com>
+ *
+ * This file is part of Snappy.
+ *
+ * Snappy is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Snappy is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Snappy.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// Composer... Is that you?
+if (file_exists('../vendor/autoload.php'))
+	require '../vendor/autoload.php';
+
+// Bootstrap Snappy
+require '../bootstrap.php';
