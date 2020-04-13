08a88043891d2e95142a75c6360bbd77adfc755b
AlexanderGW <alexgw@gmail.com>
2020-04-13 22:38:57 +0100

    Add header and cleanup

--- tests/unit/1-core.php
+++ tests/unit/1-core.php
@@ -1,12 +1,35 @@
 <?php
 
 /**
- * test snappy.php
+ * Snappy, a micro framework for PHP.
+ *
+ * @author Alexander Gailey-White <alex@gailey-white.com>
+ *
+ * This file is part of Snappy.
+ *
+ * Snappy is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Snappy is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Snappy.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Here is my attempt to convey my thought process for Snappy core, and how it should work.
+ *
+ * Class SnappyUnitTest
  *
  * @group core.unit
  */
 
-class SnappyUnitTest extends \PHPUnit\Framework\TestCase {
+class SnappyCoreUnitTest extends \PHPUnit\Framework\TestCase {
 
 	protected function setUp(): void {
 		parent::setUp();
