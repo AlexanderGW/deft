7699749be74942bb90c242a6d0f66a5b1709a73d
AlexanderGW <alexgw@gmail.com>
2020-07-03 00:04:29 +0100

    Bump to 0.8

--- deft.php
+++ deft.php
@@ -35,7 +35,7 @@ define('TIME_UTC', (time() - date('Z')));
  * Class Deft
  */
 class Deft {
-	const VERSION = '0.7';
+	const VERSION = '0.8';
 
 	const PLUGIN_LOADED = 2;
 	const PLUGIN_EXISTS = 1;
