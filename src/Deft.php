ef7d4e9808e34934a398afe9f17098e4223055d6
AlexanderGW <alexgw@gmail.com>
2020-10-18 14:32:34 +0100

    Version 0.10

--- src/Deft.php
+++ src/Deft.php
@@ -35,7 +35,7 @@ define('TIME_UTC', (time() - date('Z')));
  * Class Deft
  */
 class Deft {
-	const VERSION = '0.10-dev';
+	const VERSION = '0.10';
 
 	const PLUGIN_LOADED = 2;
 	const PLUGIN_EXISTS = 1;
